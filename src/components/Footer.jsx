0d6775f325c1ddd3fc1961195ce27c362795c94d
Justin Zhang <justinz@DESKTOP-VJ2E4S0.localdomain>
2022-11-30 20:50:51 -0500

    Initial Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div className="py-5 text-center">
+            {/* social icons */}
+            <p className="text-sm mt-2 opacity-50">
+                 Justin Zhang, {new Date().getFullYear()}
+            </p>
+        </div>
+    )
+}
+
+export default Footer; 
---
+++ src/data/portfolio.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        title: '3D Printing / Rapid Prototyping Business',
+        imgUrl: '',
+        stack: ['Ender 3','Prusa i3Mk3S+','Cura / PrusaSlicer'], 
+        link: 'https://'
+
+    },
+    {
+        title: 'Lego EV3 Robot Project',
+        imgUrl: '/assets/walle beauty shot.png',
+        stack: ['SolidWorks','AutoCAD','RobotC'], 
+        link: 'https://'
+
+    },
+    {
+        title: 'Mechanical Keyboard Design Project',
+        imgUrl: '/assets/keyboard beauty shot.png',
+        stack: ['SolidWorks','Soldering','3D Print Finishing'], 
+        link: 'https://'
+
+    },
+]
